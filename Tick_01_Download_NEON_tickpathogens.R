7d41f1697f95ec256bcb6752eaac6944acd9b7d8
Wynne Moss <wynne.moss@colorado.edu>
2020-01-30 10:50:06 -0700

    Added git ignore message

--- Tick_01_Download_NEON_tickpathogens.R
+++ Tick_01_Download_NEON_tickpathogens.R
@@ -7,7 +7,9 @@ library(neonUtilities)
 # Tick Pathogen data are in DP1.10092.001
 zipsByProduct(dpID = "DP1.10092.001", site = "all", package = "basic",
               savepath = "data_raw/") # downloads from NEON as zip
-stackByTable("filesToStack10092/") # merges zips into csv
+stackByTable("data_raw/filesToStack10092/") # merges zips into csv
 
-tickPathogen <- read.csv("filesToStack10092/stackedFiles/tck_pathogen.csv")
+tickPathogen <- read.csv("data_raw/filesToStack10092/stackedFiles/tck_pathogen.csv")
 head(tickPathogen)
+
+# add  data_raw/filesToStack10092 to .gitignore
