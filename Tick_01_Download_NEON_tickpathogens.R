81a07ba1f6b397d3a35ef5837bc8304084e263d9
Wynne Moss <wynne.moss@colorado.edu>
2020-01-30 10:24:05 -0700

    Download NEON tick pathogen data from NeonUtilities

---
+++ Tick_01_Download_NEON_tickpathogens.R
@@ -0,0 +1,13 @@
+### ABOUT
+## Script to download NEON Tick data for EDA
+## 30 Jan 2020
+## WEM
+library(neonUtilities)
+### downloading NEON data #####
+# Tick Pathogen data are in DP1.10092.001
+zipsByProduct(dpID = "DP1.10092.001", site = "all", package = "basic",
+              savepath = "data_raw/") # downloads from NEON as zip
+stackByTable("filesToStack10092/") # merges zips into csv
+
+tickPathogen <- read.csv("filesToStack10092/stackedFiles/tck_pathogen.csv")
+head(tickPathogen)
